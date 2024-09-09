44ac37a9ed6dc8dbb4b057f3186e4bcbc3c60732
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-09 16:21:57 -0600

    Add styles on the hero text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,8 +4,12 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 const Hero = () => {
   return (
     <section>
-      <div className="container mx-auto h-full items-center xl:items-start">
-        <div>text</div>
+      <div className="container mx-auto h-full flex items-center xl:items-start">
+        {/* text */}
+        <div>
+          <h1 className='text-[60px] font-bold tracking-[-0.5px] leading-none max-w-[800px] xl:max-w-max'>Boost Your Productivity Instantly</h1>
+        </div>
+        {/* img */}
         <div className='hidden xl:flex w-full max-w-[960px] mx-auto h-[520px] bg-no-repeat sticky left-0 right-0'
           style={{
             backgroundImage: "url('/hero/img.svg')",
