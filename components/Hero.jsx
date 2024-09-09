ddb140a8b72905c5f449e0ca24bd8527596fe11a
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-09 16:41:08 -0600

    Modify the styles of the text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,8 +6,10 @@ const Hero = () => {
     <section>
       <div className="container mx-auto h-full flex items-center xl:items-start">
         {/* text */}
-        <div>
-          <h1 className='text-[60px] font-bold tracking-[-0.5px] leading-none max-w-[800px] xl:max-w-max'>Boost Your Productivity Instantly</h1>
+        <div className='bg-pink-50/10 flex flex-col justify-center items-center 
+        gap-6 text-center fixed left-0 right-0 mt-24 xl:mt-[160px]'>
+          <h1 className='text-[60px] font-bold tracking-[-0.5px] leading-none max-w-[800px]
+           xl:max-w-max'>Boost Your Productivity Instantly</h1>
         </div>
         {/* img */}
         <div className='hidden xl:flex w-full max-w-[960px] mx-auto h-[520px] bg-no-repeat sticky left-0 right-0'
@@ -15,7 +17,7 @@ const Hero = () => {
             backgroundImage: "url('/hero/img.svg')",
             backgroundSize: "contain",
             backgroundPosition: "center",
-          }}>image</div>
+          }}></div>
       </div>
     </section>
   )
