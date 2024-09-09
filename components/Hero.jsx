6292b68016db89cb0498c989c4254fdfcf3e587e
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-09 15:54:26 -0600

    Add the hero image and add styles on it

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,17 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <section>Hero</section>
+    <section>
+      <div className="container mx-auto h-full items-center xl:items-start">
+        <div>text</div>
+        <div className='hidden xl:flex w-full max-w-[960px] mx-auto h-[520px] bg-no-repeat sticky left-0 right-0'
+          style={{
+            backgroundImage: "url('/hero/img.svg')",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+          }}>image</div>
+      </div>
+    </section>
   )
 }
 
