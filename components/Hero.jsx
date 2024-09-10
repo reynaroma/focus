2dc6d55e570a3a19eea42e7e038d65044923e41d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-09 18:42:42 -0600

    Add textDisplay animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
 
   const textOpacity = useTransform(scrollY, [0, 200], [1, 0]);
   const textScale = useTransform(scrollY, [0, 200], [1, 0.8]);
+  const textDisplay = useTransform(scrollY, [0, 400], ["flex", "none"]);
 
   return (
     <section className='h-screen xl:h-[1600px] overflow-x-clip relative'>
@@ -17,13 +18,13 @@ const Hero = () => {
         {/* text */}
         <motion.div className='flex flex-col justify-center items-center 
         gap-6 text-center fixed left-0 right-0 mt-24 xl:mt-[160px]' style={{
-          opacity: textOpacity,
-          scale: textScale,
-        }}>
+            opacity: textOpacity,
+            scale: textScale,
+          }}>
           <h1 className='text-[60px] font-bold tracking-[-0.5px] leading-none max-w-[800px]
            xl:max-w-max'>Boost Your Productivity Instantly</h1>
-           <p className='max-w-[680px] text-[20px] text-white/80 font-light px-8 xl:px-0 mb-2'>Streamline tasks and manage your time effortlessly with our powerful, intuitive, all in one productivity platform.</p>
-           <button>Join today</button>
+          <p className='max-w-[680px] text-[20px] text-white/80 font-light px-8 xl:px-0 mb-2'>Streamline tasks and manage your time effortlessly with our powerful, intuitive, all in one productivity platform.</p>
+          <button>Join today</button>
         </motion.div>
         {/* img */}
         <motion.div className='hidden xl:flex w-full max-w-[960px] mx-auto h-[520px] bg-no-repeat sticky left-0 right-0'
