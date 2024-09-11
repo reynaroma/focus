f73cb23c889251088c3da9c06ecf0a15f20e8143
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-11 14:25:52 -0600

    Import the buttonSedcondary and passed down the text props

--- components/ButtonSecondary.jsx
+++ components/ButtonSecondary.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ButtonSecondary = () => {
+const ButtonSecondary = ({btnText}) => {
   return (
     <button className="relative min-w-[160px] inline-flex items-center justify-center px-6 py-[15px] overflow-hidden font-medium transition duration-300 ease-out rounded-full shadow-xl group">
       <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-accent via-accent_secondary to-accent"></span>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,11 @@
+import ButtonSecondary from "./ButtonSecondary"
 
 const Header = () => {
   return (
-    <header className="h-[80px] bg-black/20 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10 flex items-center justify-between">Header</header>
+    <header className="h-[80px] bg-black/20 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10 flex items-center justify-between">
+      {/* btn */}
+      <ButtonSecondary btnText={"Get started"}/>
+    </header>
   )
 }
 
