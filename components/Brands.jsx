3574572bdf426c88884e35b1838d1ea054d4d70f
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-11 15:45:01 -0600

    Create a Brands component and import it in the main page

---
+++ components/Brands.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Brands = () => {
+  return (
+    <div>Brands</div>
+  )
+}
+
+export default Brands
