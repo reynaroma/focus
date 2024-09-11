ff3f229ff4000b7406662cc2edd64ca3fba6b892
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-11 15:05:00 -0600

    Add style on the secondary button

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,15 @@ import ButtonSecondary from "./ButtonSecondary"
 
 const Header = () => {
   return (
-    <header className="h-[80px] bg-black/20 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10 flex items-center justify-between">
-      {/* btn */}
-      <ButtonSecondary btnText={"Get started"}/>
+    <header className="h-[80px] bg-black/20 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10 flex items-center">
+      <div className="container mx-auto flex justify-between items-center px-6 xl:px-0">
+        {/* logo */}
+        <div className="">
+          logo
+        </div>
+        {/* btn */}
+        <ButtonSecondary btnText={"Get started"} />
+      </div>
     </header>
   )
 }
