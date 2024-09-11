04592b9d3a6608f6079cd8742b342c355be949ab
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-11 13:26:08 -0600

    Add initial styling on the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="h-[80px] bg-black/20 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10 flex items-center justify-between">Header</header>
   )
 }
 
