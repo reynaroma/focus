0849b43723b5018a0e0aa9ba6ec722c429ba0809
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 14:12:20 -0600

    Add the text content and fix the typo on the main div styles

--- components/Integrations.jsx
+++ components/Integrations.jsx
@@ -58,9 +58,12 @@ const icons = [
 const Integrations = () => {
   return (
     <section className="py-24 xl:py-32 min-h-[720px] xl:mt-32">
-      <div className="container mx-auto flex flex-col justify-center items-center gap-8xl:gap-16">
+      <div className="container mx-auto flex flex-col justify-center items-center gap-8 xl:gap-16">
         {/* text */}
-        <div className="">text</div>
+        <div className="">
+          <h2 className="">Unified Workflows</h2>
+          <p className="">Integrate with top apps to create a seamless, connected experience.</p>
+        </div>
         {/* icon list */}
         <div className="">icon</div>
         {/* btn */}
