2180eba06d57c5f7a68d2db7c5b68c00729683fd
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-11 19:41:45 -0600

    import motion, useEffect, useState, Image and the icon

--- components/Features.jsx
+++ components/Features.jsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { motion } from "framer-motion";;
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { FaRegCheckCircle} from 'react-icons/fa';
+
+
 const Features = () => {
   return (
     <section>Features</section>
