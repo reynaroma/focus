982c38402549278c898ee38f79bacefed3af1c2d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 16:21:09 -0600

    Add array of an object for the testimonials and style the main section

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,8 +1,49 @@
 "use client";
 
+import { motion } from "framer-motion";
+import { BiSolidQuoteLeft } from "react-icons/bi";
+
+import { fadeInOnScroll } from "@/motion/motionVariants";
+
+const testimonial = [
+  {
+    name: "John Doe",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Jane Smith",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Samuel Green",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Emily White",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Michael Brown",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Sophia Lee",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+  {
+    name: "Olivia Martinez",
+    message: "This service was incredible! It exceeded all my expectations and I couldn't be happier with the results."
+  },
+];
+
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section className="w-full xl:py-24 mb-24 xl:mb-32 flex justify-center items-center">
+      <div className="overflow-hideen">
+        {/* text */}
+        <div></div>
+      </div>
+    </section>
   )
 }
 
