315f988c62ed92faab5a59873f300c1135324a79
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 14:02:04 -0600

    Create an array og object for the icons in the integrations component

--- components/Integrations.jsx
+++ components/Integrations.jsx
@@ -4,6 +4,57 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import Button from "./Button";
 
+const icons = [
+  {
+    src: "/integrations/notion.svg",
+  },
+  {
+    src: "/integrations/slack.svg",
+  },
+  {
+    src: "/integrations/google_drive.svg",
+  },
+  {
+    src: "/integrations/intercom.svg",
+  },
+  {
+    src: "/integrations/jira.svg",
+  },
+  {
+    src: "/integrations/dropbox.svg",
+  },
+  {
+    src: "/integrations/stripe.svg",
+  },
+  {
+    src: "/integrations/zapier.svg",
+  },
+  {
+    src: "/integrations/figma.svg",
+  },
+  {
+    src: "/integrations/confluence.svg",
+  },
+  {
+    src: "/integrations/mailchimp.svg",
+  },
+  {
+    src: "/integrations/whatsapp.svg",
+  },
+  {
+    src: "/integrations/discord.svg",
+  },
+  {
+    src: "/integrations/bitbucked.svg",
+  },
+  {
+    src: "/integrations/invision.svg",
+  },
+  {
+    src: "/integrations/evernote.svg",
+  },
+]
+
 const Integrations = () => {
   return (
     <section className="py-24 xl:py-32 min-h-[720px] xl:mt-32">Integrations</section>
