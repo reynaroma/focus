1d679c8fc571b61517a9850e25c7f6da3ed4a0d2
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 14:04:47 -0600

    Create a structure of what the integrations component looks like

--- components/Integrations.jsx
+++ components/Integrations.jsx
@@ -57,7 +57,18 @@ const icons = [
 
 const Integrations = () => {
   return (
-    <section className="py-24 xl:py-32 min-h-[720px] xl:mt-32">Integrations</section>
+    <section className="py-24 xl:py-32 min-h-[720px] xl:mt-32">
+      <div className="container mx-auto">
+        {/* text */}
+        <div className="">text</div>
+        {/* icon list */}
+        <div className="">icon</div>
+        {/* btn */}
+        <div>
+          <Button btnText="See all" />
+        </div>
+      </div>
+    </section>
   )
 }
 
