6411ced7b7cbd006f984b12a5801f11d35443d50
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 20:14:37 -0600

    Add structure or outline of the trial component and style it.

--- components/Trial.jsx
+++ components/Trial.jsx
@@ -1,8 +1,14 @@
 "use client";
+import { motion } from "framer-motion";
+import { fadeInUpSpring } from "@/motion/motionVariants";
 
 const Trial = () => {
   return (
-    <section>Trial</section>
+    <section className="xl:mb-32 w-full">
+      <div className="py-24 w-full xl:max-w-[1140px] mx-auto min-h-[300px] bg-gradient-to-r from-accent via-accent_secondary to-accent h-full flex items-center xl:rounded-2xl">
+        <div>trial</div>
+      </div>
+    </section>
   )
 }
 
