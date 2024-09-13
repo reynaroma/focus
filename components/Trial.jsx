19d5448144d8939dc5586703bfc0f19d8ffefb01
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 20:17:30 -0600

    Add text for the main content of the component

--- components/Trial.jsx
+++ components/Trial.jsx
@@ -6,7 +6,15 @@ const Trial = () => {
   return (
     <section className="xl:mb-32 w-full">
       <div className="py-24 w-full xl:max-w-[1140px] mx-auto min-h-[300px] bg-gradient-to-r from-accent via-accent_secondary to-accent h-full flex items-center xl:rounded-2xl">
-        <div>trial</div>
+        <div>
+          <div>
+            {/* text */}
+            <div>
+              <h2 className="">Start your 30-day free trial</h2>
+              <p>Experience the full power of our platform with a 30-day free trial. No credit card required.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
