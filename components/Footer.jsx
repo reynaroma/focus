b3bb2d2be4a913651ef85f8582bfac71f13c7050
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-12 21:04:10 -0600

    Add an array of icon objects for footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,37 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Logo from './Logo';
 
+const icons = [
+  {
+    src: "/footer/1.svg",
+    href: "",
+  },
+  {
+    src: "/footer/2.svg",
+    href: "",
+  },
+  {
+    src: "/footer/3.svg",
+    href: "",
+  },
+  {
+    src: "/footer/4.svg",
+    href: "",
+  },
+  {
+    src: "/footer/5.svg",
+    href: "",
+  },
+  {
+    src: "/footer/6.svg",
+    href: "",
+  },
+  {
+    src: "/footer/7.svg",
+    href: "",
+  },
+];
+
 const Footer = () => {
   return (
     <footer>Footer</footer>
